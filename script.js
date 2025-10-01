a0ad2d348c72fb817fcbf4ef470959f6b226a39f
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-10-01 18:46:28 +0200

    Implement displayTasks function and fix displaying of the task

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Retrieve todo from local storage or initialize an empty array
-let todo = JSON.parse(localStorage.getItem('todo')) || [];
-const todoInput = document.getElementById('todoInput');
-const todoList = document.getElementById('todoList');
-const todoCount = document.getElementById('todoCount');
-const addButton = document.querySelector('.btn');
-const deleteButton = document.getElementById('deleteButton');
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
 
 // Initialize
-document.addEventListener('DOMContentLoaded', function() {
-    addButton.addEventListener('click', addTask());
-    todoInput.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
+document.addEventListener("DOMContentLoaded", function () {
+    addButton.addEventListener("click", addTask);
+    todoInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
             event.preventDefault();
             addTask();
         }
-     });
-    deleteButton.addEventListener('click', deleteAllTasks);
+    });
+    deleteButton.addEventListener("click", deleteAllTasks);
     displayTasks();
 });
 
@@ -27,19 +27,36 @@ function addTask() {
             disabled: false,
         });
         savetoLocalStorage();
-        todoInput.value = '';
+        todoInput.value = "";
         displayTasks();
     }
 }
 
-function deleteAllTasks() {
-
-}
+function deleteAllTasks() {}
 
 function displayTasks() {
-
+    todoList.innerHTML = "";
+    todo.forEach((item, index) => {
+        const p = document.createElement("p");
+        p.innerHTML = `
+            <div class="todo-container">
+                <input type="checkbox" class="todo-checkbox" id="input-${index}" ${
+            item.disabled ? "checked" : ""
+        } />
+                <p id="todo-${index}" class="${
+            item.disabled ? "disabled" : ""
+        }" onclick="editTask(${index})">
+                    ${item.text}
+                </p>
+            </div>
+        `;
+        p.querySelector(".todo-checkbox").addEventListener("change", () => {
+            toggleTask(index);
+        });
+        todoList.appendChild(p);
+    });
 }
 
 function savetoLocalStorage() {
-    localStorage.setItem('todo', JSON.stringify(todo));
+    localStorage.setItem("todo", JSON.stringify(todo));
 }
